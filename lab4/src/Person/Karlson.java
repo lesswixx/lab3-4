bbaa90d8dd07793b7f79cbde2e6c7dca15f3279d
lesswixx <98524879+lesswixx@users.noreply.github.com>
2022-01-28 14:54:39 +0300

    Update Karlson.java

--- lab4/src/Person/Karlson.java
+++ lab4/src/Person/Karlson.java
@@ -1,45 +1,53 @@
 package Person;
 
 import Enum.MoodEnum;
-import Interface.PropellerInterface;
+import Interface.FlyAble;
 import core.ConditionalAbstract;
 import exception.BatteryException;
 
-public class Karlson extends ConditionalAbstract implements PropellerInterface {
+public class Karlson extends ConditionalAbstract implements FlyAble {
     private Vint vint = new Vint();
+    private Battery battery = new Battery();
 
     public Karlson(String name, int age, MoodEnum mood, boolean conditional, String street, String house) {
-        super(name, age, mood, conditional,street, house);
+        super(name, age, mood, conditional, street, house);
 
     }
-    private class Vint{     // Вложенный нестатический класс(non-static)
-        public void startVint(){
+
+    private class Vint {     // Вложенный нестатический класс(non-static)
+        public void startVint() {
             System.out.println(Karlson.this.getName() + " запустил пропеллер");
         }
 
     }
 
-    public static class Battery  {      // Вложенный статический класс (static)
+    public void start() {
+        vint.startVint();
+    }
+
+    private class Battery {      // Вложенный нестатический класс(non-static)
         public int charge(int percent) {
-            for (; percent + 20 <= 100; percent = percent+20) {
+            for (; percent + 20 <= 100; percent = percent + 20) {
                 System.out.println("Батарея заряжается, уровень заряда: " + percent + " %");
             }
             System.out.println("Батарея имеет максимальный заряд: " + percent + " %");
             return percent;
         }
-        public void chekCharge(int percent) throws BatteryException {
 
-            if(charge(percent) > 100) {
+        public void chekCharge(int percent) throws BatteryException {
+            if ((charge(percent) > 100)) {
                 throw new BatteryException("Батарея не может иметь заряд больше 100%");
             }
         }
     }
 
-    public void start(){
-        vint.startVint();
+    public void Charge(int percent) {
+        battery.chekCharge(percent);
     }
+
+
     @Override
-    public void conditional(){
+    public void conditional() {
         if (getConditional()) {
             System.out.println(getName() + " болтает ножками в воздухе");
         } else {
@@ -61,8 +69,9 @@ public class Karlson extends ConditionalAbstract implements PropellerInterface {
     public String getHouse() {
         return house;
     }
+
     @Override
-    public void getStreet() {
+    public String getStreet() {
         return street;
     }
 }
