acc80770833d2e74032886f158e641eec2fc4462
lesswixx <98524879+lesswixx@users.noreply.github.com>
2022-01-28 14:54:13 +0300

    Update BigHuman.java

--- lab4/src/Person/BigHuman.java
+++ lab4/src/Person/BigHuman.java
@@ -4,14 +4,28 @@ import Enum.MoodEnum;
 import core.PersonAbstract;
 
 
-public class BigHuman extends PersonAbstract {
 
-    public BigHuman(String name, int age, MoodEnum mood, String street, String house) {
+
+public class BigHuman extends PersonAbstract {
+    private static int point = 100;
+    public BigHuman(String name, int age, MoodEnum mood, String street, String house, int point) {
         super(name, age, mood, street, house);
+        this.point = point;
 
 
     }
+    public static class Reputation{
+        public int points(){
+            return point;
+        }
+        public int lowPoints(){
+            return point - 20;
+        }
+        public int hightPoints(){
+            return point + 20;
 
+        }
+    }
 
     @Override
     public void walk() {
